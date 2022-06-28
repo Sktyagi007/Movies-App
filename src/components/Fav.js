0c534f822e9b40c7720e6b89de5ae6aa40369416
Sktyagi007 <sarthak765888@gmail.com>
2022-06-28 23:38:04 +0530

    filtering movies completed

--- src/components/Fav.js
+++ src/components/Fav.js
@@ -3,25 +3,70 @@ import {movies} from '../movieData'
 
 
 class Fav extends Component{
+    constructor(){
+        super();
+        this.state = {
+            genres:[],
+            currgenre:'All genres',
+            movies:[]
+        }
+    }
+    componentDidMount(){
+        let genreIds = { 28: "Action", 12: "Adventure", 16: "Animation", 35: "Comedy", 80: "Crime", 99: "Documentary", 18: "Drama", 10751: "Family", 14: "Fantasy", 36: "History", 27: "Horror", 10402: "Music", 9648: "Mystery", 10749: "Romance", 878: "Science Fiction", 10770: "TV Movie", 53: "Thriller", 10752: "War", 37: "Western"}
+        let data = JSON.parse(localStorage.getItem("movies-app") || '[]'); //movies
+        let tempArr = [];
+        tempArr.push("All genres")
+        data.map((movieObj)=>{
+            if(!tempArr.includes(genreIds[movieObj.genre_ids[0]])){
+                tempArr.push(genreIds[movieObj.genre_ids[0]])
+            }
+        })
+    
+        this.setState({
+            movies:[...data],
+            genres:[...tempArr]
+        })
+    }
+
+    handleChangeGenre = (genre)=>{
+        this.setState({
+            currgenre:genre,
+        },this.filterMovies)
+    }
+
+    filterMovies = ()=>{
+        let genreIds = { 28: "Action", 12: "Adventure", 16: "Animation", 35: "Comedy", 80: "Crime", 99: "Documentary", 18: "Drama", 10751: "Family", 14: "Fantasy", 36: "History", 27: "Horror", 10402: "Music", 9648: "Mystery", 10749: "Romance", 878: "Science Fiction", 10770: "TV Movie", 53: "Thriller", 10752: "War", 37: "Western"}
+        let data = JSON.parse(localStorage.getItem("movies-app") || '[]');
+        if(this.state.currgenre == 'All genres'){
+            this.setState({
+                movies:[...data]
+            })
+        }else{
+            let filterMovies = data.filter((movieObj) => genreIds[movieObj.genre_ids[0]] == this.state.currgenre);
+            this.setState({
+                movies:[...filterMovies]
+            })
+        }
+    }
+
+
     render(){
         const moviesArr = movies.results
         // console.log(moviesArr)
-        let genreIds = { 28: "Action", 12: "Adventure", 16: "Animation", 35: "Comedy", 80: "Crime", 99: "Documentary", 18: "Drama", 10751: "Family", 14: "Fantasy", 36: "History", 27: "Horror", 10402: "Music", 9648: "Mystery", 10749: "Romance", 878: "Science Fiction", 10770: "TV Movie", 53: "Thriller", 10752: "War", 37: "Western"}
-        let tempArr = [];
-        tempArr.push("All genres");
-        moviesArr.map((movieObj)=>{
-            if(!tempArr.includes(genreIds[movieObj.genre_ids[0]])){
-                tempArr.push(genreIds[movieObj.genre_ids[0]]);
-            }
-        })
+        let genreIds = { 28: "Action", 12: "Adventure", 16: "Animation", 35: "Comedy", 80: "Crime", 99: "Documentary", 18: "Drama", 10751: "Family", 14: "Fantasy", 36: "History", 27: "Horror", 10402: "Music", 9648: "Mystery", 10749: "Romance", 878: "Science Fiction", 10770: "TV Movie", 53: "Thriller", 10752: "War", 37: "Western"}    
         return(
             <div className="container">
             <div className="row">
                 <div className="col-3">
                     <ul className="list-group genre-selector">
                         {
-                            tempArr.map((genre)=>(
-                                <li className="list-group-item">{genre}</li>
+                            this.state.genres.map((genre)=>(
+                                this.state.currgenre == genre ?(
+                                    <li className="list-group-item active">{genre}</li>
+                                ):(
+                                    <li className="list-group-item" onClick={() =>this.handleChangeGenre(genre)}>{genre}</li>
+                                )
+                                
                             ))
                         }
                         {/* <li className="list-group-item">Favourites</li>
@@ -48,7 +93,7 @@ class Fav extends Component{
                             </thead>
                             <tbody>
                                 {
-                                    moviesArr.map((movieEle)=>(
+                                    this.state.movies.map((movieEle)=>(
                                         <tr>
                                             <th scope="row"><img  style={{width:"8rem",padding:"1rem"}} src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`}/>{movieEle.title}</th>
                                             <td>{genreIds[movieEle.genre_ids[0]]}</td>
